17acc03423fc4924a6c9ecc3d81164f9b96ce0fa
Sunfury <tidus_1992@hotmail.it>
2014-12-25 15:38:09 +0100

    Sistemato i package

--- app/src/main/java/com/example/simello/controller/punteggi/Punteggio.java
+++ app/src/main/java/com/example/simello/controller/punteggi/Punteggio.java
@@ -7,11 +7,17 @@ public class Punteggio
 {
     private int valore;
 
+    public Punteggio()
+    {
+        this.valore = 0;
+    }
+
     public int getValore() {
         return valore;
     }
 
     public void updatePoints() {
        //@Todo: "chiamo script per aggiornamento punti"
+        this.valore += 100;
     }
 }
--- app/src/main/java/com/example/simello/guanxy/MainActivity.java
+++ app/src/main/java/com/example/simello/guanxy/MainActivity.java
@@ -21,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.simello.controller.punteggi.Punteggio;
 import com.example.simello.controller.punteggi.Utente;
 import com.viewpagerindicator.TabPageIndicator;
 import com.viewpagerindicator.TitlePageIndicator;
@@ -189,12 +190,17 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
             //Qui verrà implementato tutto il lavoro di Simone,
             //Quindi con creazione classe ControllerPunteggio dal quale verranno ricavati i campi da stampare
             //Quando sarà da ripetere per il pulsante Chiedi Aiuto, utilizza TabActivity
-            TextView t = (TextView) rootView.findViewById(R.id.user);
+            //Problema, come ricava i campi Simone?? La classe di Simone nn è activity e non può esserla, quindi come si fa?
+            TextView tUser = (TextView) rootView.findViewById(R.id.user);
+            TextView tPunti = (TextView) rootView.findViewById(R.id.punti);
+            Punteggio punti = new Punteggio();
             Utente user = new Utente("Sunfury");
-            t.setText(user.getNome());
+            tUser.setText(user.getNome());
+            punti.updatePoints();
+            tPunti.setText(""+punti.getValore());
             return rootView;
         }
-
+            //Ricontrolla i metodi onResume() del fragment
 
     }
 
