09489cdd562c6feda91675cc95d8bfe82aece1da
Sunfury <tidus_1992@hotmail.it>
2015-03-01 15:44:27 +0100

    Inizio Registrazione part 1 senza push

--- app/src/main/java/com/example/simello/guanxy/MappaActivity.java
+++ app/src/main/java/com/example/simello/guanxy/MappaActivity.java
@@ -1,11 +1,18 @@
 package com.example.simello.guanxy;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by simello e sunfury on 01/03/15.
  */
 public class MappaActivity extends Activity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
+    }
 }
