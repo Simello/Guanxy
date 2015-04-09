5cb85b7d9d95e6bcbf3e76e3fad37c23c41ef52f
Sunfury <tidus_1992@hotmail.it>
2015-04-09 11:37:47 +0200

    OK schermate!

--- app/src/main/java/com/example/simello/classiServer/MessageByTimeInput.java
+++ app/src/main/java/com/example/simello/classiServer/MessageByTimeInput.java
@@ -14,51 +14,33 @@ public class MessageByTimeInput extends BaseInput {
      */
     private static final long serialVersionUID = -4168765750832562881L;
 
-
     private BigInteger idHelp;
 
     private String idUser1;
-
-    private String idUser2;
-
     private Date time;
 
     public BigInteger getIdHelp() {
         return idHelp;
     }
-
-    public void setIdHelp(BigInteger idHelp) {
+    public void setIdHelp( BigInteger idHelp ) {
         this.idHelp = idHelp;
     }
-
     public String getIdUser1() {
         return idUser1;
     }
-
-    public void setIdUser1(String idUser1) {
+    public void setIdUser1( String idUser1 ) {
         this.idUser1 = idUser1;
-    }
-
-    public String getIdUser2() {
-        return idUser2;
-    }
-
-    public void setIdUser2(String idUser2) {
-        this.idUser2 = idUser2;
     }
 
     public Date getTime() {
         return time;
     }
-
-    public void setTime(Date time) {
+    public void setTime( Date time ) {
         this.time = time;
     }
-
     @Override
     public String toString() {
-        return "MessageByTimeInput [idHelp=" + idHelp + ", idUser1=" + idUser1 + ", idUser2=" + idUser2 + ", time=" + time + "]";
+        return "MessageByTimeInput [idHelp=" + idHelp + ", idUser1=" + idUser1 + ", time=" + time + "]";
     }
-
 
 }
